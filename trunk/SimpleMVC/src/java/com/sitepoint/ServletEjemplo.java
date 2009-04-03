be35be24fbd6019031fb454665982078256984a3
EdVelez.G <EdVelez.G@cf767502-2092-11de-b56b-c9554f25afe1>
2009-04-03 21:10:43 +0000

    New import

---
+++ trunk/SimpleMVC/src/java/com/sitepoint/ServletEjemplo.java
@@ -0,0 +1,85 @@
+package com.sitepoint;
+
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Andr�s Felipe
+ *
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ServletEjemplo extends HttpServlet {
+
+    public void init() throws ServletException {
+        super.init();
+
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String salida = null;
+        DataInputStream din = null;
+        PrintWriter out = null;
+
+        ByteArrayOutputStream bout = null;
+        DataOutputStream dos = null;
+
+
+        try {
+            out = response.getWriter();
+            ServletInputStream in = request.getInputStream();
+            din = new DataInputStream(in);
+        } catch (IOException io) {
+            System.out.println("Se ha producido una excepcion");
+        }
+
+        try {
+//            Persona persona = new Persona();
+
+            String nombre = din.readUTF();
+            System.out.println("Nombre " + nombre);
+//            persona.setNombre(nombre);
+
+            if (nombre != null) {
+                ToDoList toDoList = (ToDoList) getServletContext().getAttribute("toDoList");
+                toDoList.addItem(nombre);
+            }
+
+            String apellido = din.readUTF();
+            System.out.println("Apellido " + apellido);
+//            persona.setApellido(apellido);
+
+//            if (guardarPersona(persona)) {
+            bout = new ByteArrayOutputStream();
+            dos = new DataOutputStream(bout);
+            dos.writeUTF("ok");
+            byte[] salidaB = bout.toByteArray();
+            salida = new String(salidaB);
+//            }
+
+            out.print(salida);
+            out.flush();
+
+            int contentLength = salida.length();
+
+            //System.out.println("ContentLength= " + contentLength);
+            response.setContentLength(contentLength);
+        } catch (IOException ae) {
+            ae.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        out.print(salida);
+    }
+
+//    public boolean guardarPersona(Persona persona) {
+//        System.out.println("Guardando Persona...");
+//        System.out.println("Nombre:   " + persona.getNombre());
+//        System.out.println("Apellido:     " + persona.getApellido());
+//
+//        return true;
+//    }
+}
