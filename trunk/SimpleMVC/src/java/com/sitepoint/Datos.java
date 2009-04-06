fd22aa5996dcbabb9493eebec82d3c6c93e9bf6f
EdVelez.G <EdVelez.G@cf767502-2092-11de-b56b-c9554f25afe1>
2009-04-06 21:16:20 +0000

    woops forgot this file, fixes previous version

---
+++ trunk/SimpleMVC/src/java/com/sitepoint/Datos.java
@@ -0,0 +1,115 @@
+package com.sitepoint;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Victor
+ */
+import java.util.Vector;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Victor
+ */
+public class Datos {
+
+    private int id;
+    private String nombre;
+    private int tel;
+    private String dir;
+    private String intra;
+    private String intranet;
+    private String acceso;
+    private String imagen;
+    private Vector aficiones;
+    private int pos = 0;
+
+    public Datos(String nombre, int tel) {
+        this.nombre = nombre;
+        this.tel = tel;
+        this.dir = "n/a";
+        this.intra = "n/a";
+        this.intranet = "n/a";
+        this.acceso = "n/a";
+        this.imagen="n/a";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public String getIntra() {
+        return intra;
+    }
+
+    public String getIntranet() {
+        return intranet;
+    }
+
+    public String getAcceso() {
+        return acceso;
+    }
+    public String getImagen(){
+        return imagen;
+    }
+     public void setImagen(String imagen){
+        this.imagen=imagen;
+    }
+//    public Vector getAficiones() {
+//        return aficiones;
+//    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public void setIntra(String intra) {
+        this.intra = intra;
+    }
+
+    public void setIntranet(String intranet) {
+        this.intranet = intranet;
+    }
+
+    public void setAcceso(String acceso) {
+        this.acceso = acceso;
+    }
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public void setAficiones(Vector aficiones) {
+        this.aficiones = aficiones;
+    }
+    public int getId() {
+            return 5;
+    }
+
+    public String toString() {
+            return getNombre() + " -- " + getTel();
+    }
+}
